92a3e69ca73e12b97dbff385192aa077c6890a6c
Hasself16 <141966592+Hasself16@users.noreply.github.com>
2023-08-27 21:05:54 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -1,12 +1,16 @@
 from televisores.tv import TV
 
 class Control:
+    nControl=0
+    def __init__(self):
+        Control.nControl += 1
+    
     def setTv(self, tv):
         self.tv=tv
     
     def getTv(self):
         return self.tv
     
-    def enlazar(self, tv):
+    def enlazar(cls, self, tv):
         self.tv=tv
-        TV.setControl(self)
+        TV.setControl(cls.nControl)
