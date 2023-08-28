2cf197680a2365db07e17561dca68e0dbb5ca90c
Hasself16 <141966592+Hasself16@users.noreply.github.com>
2023-08-27 21:52:16 -0500

    metodos control

--- televisores/control.py
+++ televisores/control.py
@@ -11,3 +11,35 @@ class Control:
     def enlazar(self, tv):
         self.tv=tv
         tv.setControl(self)
+
+    def turnOn(self):
+        if self.tv:
+            self.tv.turnOn()
+
+    def turnOff(self):
+        if self.tv:
+            self.tv.turnOff()
+
+    def canalUp(self):
+        if self.tv:
+            self.tv.canalUp()
+
+    def canalDown(self):
+        if self.tv:
+            self.tv.canalDown()
+
+    def volumenUp(self):
+        if self.tv:
+            self.tv.volumenUp()
+
+    def volumenDown(self):
+        if self.tv:
+            self.tv.volumenDown()
+
+    def setCanal(self, canal):
+        if self.tv:
+            self.tv.setCanal(canal)
+
+    def setVolumen(self, volumen):
+        if self.tv:
+            self.tv.setVolumen(volumen)
